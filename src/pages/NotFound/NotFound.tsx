8298e7654f177adda69472e35ff041bf30e72bb4
cheerfulperson <yagor.baradavkin@gmail.com>
2022-08-13 17:49:26 +0300

    feat: add not found page

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import './NotFound.scss'
+
+function NotFound() {
+    return (
+        <section className='not-found'><h1>Not Found</h1></section>
+    )
+}
+
+export default NotFound;
