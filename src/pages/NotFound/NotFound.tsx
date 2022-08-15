f8def85dfd849dbdb479a53b902caf5e78552ced
cheerfulperson <yagor.baradavkin@gmail.com>
2022-08-15 15:30:38 +0300

    feat: add localSotrage hook

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
+import { Link } from 'react-router-dom';
 import './NotFound.scss'
 
 function NotFound() {
     return (
-        <section className='not-found'><h1>Not Found</h1></section>
+        <section className='not-found'><h1>Not Found</h1> <Link to=''>home</Link></section>
     )
 }
 
