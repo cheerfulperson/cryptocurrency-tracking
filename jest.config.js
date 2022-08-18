36264c6450b44e9e346ebc5e32791f9354047348
cheerfulperson <yagor.baradavkin@gmail.com>
2022-08-19 02:11:35 +0300

    feat: implement componts tests

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,10 @@ module.exports = {
   setupFilesAfterEnv: ['@testing-library/react', '@testing-library/jest-dom'],
   testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleNameMapper: {
+    'd3': '<rootDir>/node_modules/d3/dist/d3.min.js',
+    '^d3-(.*)$': `d3-$1/dist/d3-$1`,
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
+  transformIgnorePatterns: ['/node_modules/(?!d3|d3-array|internmap|delaunator|robust-predicates)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 }
--- src/tests/components/components.test.tsx
+++ src/tests/components/components.test.tsx
@@ -5,6 +5,14 @@ import Adapter from 'enzyme-adapter-react-16'
 import Button from '../../components/Button/Button'
 import Input from '../../components/Input/Input'
 import Avatar from '../../components/Avatar/Avatar'
+import Spinner from '../../components/Spinner/Spinner'
+import List from '../../components/List/List'
+import ListItem from '../../components/ListItem/ListItem'
+import Modal from '../../components/Modal/Modal'
+import { BarChart } from '../../components/BarChart/BarChart'
+import { CryptoHistory } from '../../models/crypto.models'
+import PieChart from '../../components/PieChart/PieChart'
+import { UsersCrypto } from '../../models/user.model'
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -50,12 +58,12 @@ describe('Input', () => {
 })
 
 describe('Avatar', () => {
-  test('Should display a input with class name', async () => {
+  test('Should display a avatar with image', async () => {
     const src = './app.png'
     const input = shallow(<Avatar src={src} />)
     expect(input.find('img').props().src).toBe(src)
   })
-  test('Should display right label', async () => {
+  test('Should click avatar', async () => {
     const button = shallow(
       <Avatar
         src=''
@@ -65,5 +73,186 @@ describe('Avatar', () => {
       />,
     )
     button.find('button').simulate('click')
+  })
+})
+
+describe('Spinner', () => {
+  test('Should display Spinner with middle size and warn theme', async () => {
+    const classNameSize = 'loader-middle'
+    const classNameTheme = 'loader-middle'
+    const spinner = shallow(<Spinner size='middle' theme='warn' />)
+
+    expect(spinner.hasClass(classNameSize)).toBe(true)
+
+    expect(spinner.hasClass(classNameTheme)).toBe(true)
+
+    expect(spinner.type()).toBe('article')
+  })
+})
+
+describe('List', () => {
+  const listItems = ['car', 'ship', 'tank']
+  const children = listItems.map((str) => <ListItem key={str}>{<span>{str}</span>}</ListItem>)
+  let spinner: Enzyme.ShallowWrapper
+
+  beforeAll(() => {
+    spinner = shallow(<List>{children}</List>)
+  })
+  test('Should display list', async () => {
+    expect(spinner.exists()).toBe(true)
+  })
+
+  test('Should display list with children', async () => {
+    expect(spinner.contains(children)).toBe(true)
+  })
+})
+
+describe('Modal window', () => {
+  const children = () => (
+    <>
+      <h1 className='title'>Modal</h1>
+      <p className='text'>text</p>
+    </>
+  )
+  const mockCallBack = jest.fn()
+
+  let modal: Enzyme.ShallowWrapper
+
+  beforeAll(() => {
+    modal = shallow(
+      <Modal isOpen={true} onChange={mockCallBack}>
+        <div>{children}</div>
+      </Modal>,
+    )
+  })
+
+  test('Should open modal window', () => {
+    expect(modal.hasClass('modal-hide')).toBe(true)
+  })
+
+  test('Should close modal window', () => {
+    const element = 'section'
+    modal.find('section').simulate('click', {
+      target: {
+        closest(str: string) {
+          return str === element
+        },
+      },
+    })
+    expect(mockCallBack.mock.calls[0][0]).toBe(false)
+  })
+
+  test('Should contain children', () => {
+    const element = modal.find('.title')
+    expect(!!element).toBe(true)
+  })
+})
+
+describe('Bar chart', () => {
+  const history: CryptoHistory[] = [
+    {
+      priceUsd: '112.222',
+      time: Date.now(),
+    },
+    {
+      priceUsd: '112.222',
+      time: Date.now(),
+    },
+    {
+      priceUsd: '112.222',
+      time: Date.now(),
+    },
+    {
+      priceUsd: '112.222',
+      time: Date.now(),
+    },
+  ]
+  let barChart: Enzyme.ShallowWrapper
+  const height = 500
+
+  beforeAll(() => {
+    barChart = shallow(
+      <BarChart data={history} height={height} margin={{ top: 0, bottom: 0, left: 0, right: 0 }} />,
+    )
+  })
+
+  test('Should display chart with height', async () => {
+    const svg = barChart.find('svg')
+    expect(svg.html().includes(`${height}`)).toBe(true)
+  })
+
+  test('Should display chart with data', async () => {
+    expect(!!barChart.find('rect')).toBe(true)
+  })
+})
+
+describe('Pie chart', () => {
+  const cryptoData: UsersCrypto[] = [
+    {
+      amount: 10,
+      purchasePrice: 100,
+      crypto: {
+        id: 'bitcoin',
+        symbol: 'BTC',
+        name: 'Bitcoin',
+        priceUsd: '23382.1649630199587733',
+        marketCapUsd: '447187552535.4909426529311348',
+        changePercent24Hr: '-0.0539484513410834',
+        maxSupply: '222',
+        rank: '1',
+        supply: 1,
+        volumeUsd24Hr: '222',
+        vwap24Hr: '2222',
+      },
+    },
+    {
+      amount: 10,
+      purchasePrice: 100,
+      crypto: {
+        id: 'bitcoin',
+        symbol: 'BTC',
+        name: 'Bitcoin',
+        priceUsd: '23382.1649630199587733',
+        marketCapUsd: '447187552535.4909426529311348',
+        changePercent24Hr: '-0.0539484513410834',
+        maxSupply: '222',
+        rank: '1',
+        supply: 1,
+        volumeUsd24Hr: '222',
+        vwap24Hr: '2222',
+      },
+    },
+    {
+      amount: 10,
+      purchasePrice: 100,
+      crypto: {
+        id: 'bitcoin',
+        symbol: 'BTC',
+        name: 'Bitcoin',
+        priceUsd: '23382.1649630199587733',
+        marketCapUsd: '447187552535.4909426529311348',
+        changePercent24Hr: '-0.0539484513410834',
+        maxSupply: '222',
+        rank: '1',
+        supply: 1,
+        volumeUsd24Hr: '222',
+        vwap24Hr: '2222',
+      },
+    },
+  ]
+  let pieChart: Enzyme.ShallowWrapper
+  const height = 500
+
+  beforeAll(() => {
+    pieChart = shallow(<PieChart data={cryptoData} height={height} />)
+  })
+
+  test('Should display chart with height', async () => {
+    const svg = pieChart.find('svg')
+    expect(svg.html().includes(`${height}`)).toBe(true)
+  })
+
+  test('Should display pie chart with data', async () => {
+    expect(!!pieChart.find('path')).toBe(true)
   })
 })
